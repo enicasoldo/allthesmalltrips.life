fb5722b778b4eb3a97a2ca4833aa0eacc420e575
Mario Lamacchia <mlamacchia@hubspot.com>
2017-10-30 18:43:24 +0000

    Implement infinite scrolling

---
+++ assets/js/paginate.js
@@ -0,0 +1,34 @@
+(function ($) {
+  "use strict";
+
+  var isLoading = false;
+  var currentPage = 1;
+  var trigger;
+
+  function handleScroll() {
+    if (trigger.offset().top < window.innerHeight + window.scrollY) {
+      loadPage();
+    }
+  }
+
+  function loadPage() {
+    isLoading = true;
+    currentPage++;
+    $.get('/page' + currentPage).then(function(html) {
+      $(html).find('article').each(function() {
+        trigger.prepend(this);
+      })
+      isLoading = false;
+    }, function() {
+      $(document).off('scroll', handleScroll);
+    });
+  }
+
+  $(document).ready(function(){
+    trigger = $('.js--pagination');
+    if (trigger.length) {
+      $(document).on('scroll', handleScroll);
+    }
+  });
+
+}(jQuery));
